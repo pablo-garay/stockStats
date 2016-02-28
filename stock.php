6a9646e1d24f613f7b0fc0da74ed124645359aa8
Pablo Garay <pgaray@roshka.com>
2016-02-27 20:26:38 -0800

    first commit

---
+++ stock.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <div class="body-content" style="text-align: center; padding: 15em;">
+        <div style="margin-bottom: 3em;">
+
+			<h1><i>Stock Search</i></h1>
+
+			<form method="POST">
+				Company Name or Symbol: <input type="text" name="" placeholder="Enter company name e.g. Apple"
+				required pattern="^[a-zA-Z0-9][a-zA-Z0-9 ]*$" > </input>
+				<br />
+				<input type="submit" value="Search" autofocus></input>
+				<input type="reset" value="Clear"></input>
+				<br />	
+			</form>
+			<a href="http://www.markit.com/product/markit-on-demand">Powered by Markit on Demand</a>
+		</div>
+	</div>
+
+</body>
+</html>
