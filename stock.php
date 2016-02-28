8066276c2f980ce5cac017fd405b275015978479
Pablo Garay <pgaray@roshka.com>
2016-02-27 22:45:02 -0800

    Added php form handling

--- stock.php
+++ stock.php
@@ -7,9 +7,11 @@
 
 			<h1><i>Stock Search</i></h1>
 
-			<form method="POST">
-				Company Name or Symbol: <input type="text" name="" placeholder="Enter company name e.g. Apple"
-				required pattern="^[a-zA-Z0-9][a-zA-Z0-9 ]*$" > </input>
+			<form method="POST" action="">
+				<label for="input">Company Name or Symbol: </label>
+				<input type="text" id="input" name="input" placeholder="Enter company name e.g. Apple"
+				required pattern="^[a-zA-Z0-9][a-zA-Z0-9 ]*$" 
+				value="<?php if (isset($_POST["input"])) echo htmlspecialchars($_POST['input']); ?>" > </input>
 				<br />
 				<input type="submit" value="Search" autofocus></input>
 				<input type="reset" value="Clear"></input>
@@ -18,6 +20,15 @@
 			<a href="http://www.markit.com/product/markit-on-demand">Powered by Markit on Demand</a>
 		</div>
 	</div>
+
+	<?php
+		if(isset($_POST["input"])){
+			print_r($_POST);
+		} else {
+			echo "ccc";
+		}
+		echo "hello world!";
+	?>
 
 </body>
 </html>
