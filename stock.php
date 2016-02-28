ce1550ace67bf76b5e5b7d57184413423bc9737e
Pablo Garay <pgaray@roshka.com>
2016-02-28 15:31:42 -0800

    Added simpleXML code to consume lookup web service and provide functionality to create stock table

--- stock.php
+++ stock.php
@@ -1,5 +1,18 @@
 <!DOCTYPE html>
 <html>
+
+<head>
+<style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+th, td {
+    padding: 5px;
+}
+</style>
+</head>
+
 <body>
 
     <div class="body-content" style="text-align: center; padding: 15em;">
@@ -23,12 +36,61 @@
 
 	<?php
 		if(isset($_POST["input"])){
-			print_r($_POST);
+			print_r($_POST);			
 		} else {
 			echo "ccc";
 		}
-		echo "hello world!";
-	?>
+		
+
+		/* Output debugging data */
+		// $xml = file_get_contents("http://dev.markitondemand.com/MODApis/Api/v2/Lookup/xml?input=APPL");
+		// echo "<br />";
+		// print_r($xml);
+
+		/* XML */
+		$xmlElement = simplexml_load_file('http://dev.markitondemand.com/MODApis/Api/v2/Lookup/xml?input=' . $_POST["input"]);	 
+		echo "<br /><br />";
+	    
+	    if ($xmlElement->count() == 0) {
+		    //it's empty
+		    echo "No Record has been found";
+		    echo "<br />";
+		    // print_r($xmlElement);
+		}
+		else {
+		    //XML object has children
+		    echo "no empty";
+
+		    echo "<br />";
+
+		    echo '<table style="width:100%">';
+		    echo '<tr><th>Name</th><th>Symbol</th><th>Exchange</th><th>Details</th></tr>';
+
+		    foreach ($xmlElement->LookupResult as $result) {
+		    	echo '<tr>';
+			   // print_r($result);
+		    	echo '<th>' . $result->Name . '</th>';
+		    	echo '<th>' . $result->Symbol . '</th>';
+		    	echo '<th>' . $result->Exchange . '</th>';
+		    	echo '<th>' . '<a href="http://www.nba.com\">More Info</a>' . '</th>';
+
+			   echo "<br />";
+			   echo '</tr>';
+			}
+			echo "</table>";
+
+
+		    // print_r($xmlElement);
+		}
+
+	 //    echo "<br /><br />";
+	 //    print_r($xmlElement->Status);
+
+		// /* JSON */
+		// $json = file_get_contents("http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=AAPL");
+		// echo "<br /><br />";
+		// print_r($json);		
+	?>	
 
 </body>
 </html>
